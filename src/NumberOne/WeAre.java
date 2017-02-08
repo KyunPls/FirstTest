a7ad42e12a711dcf84d8e9eab0565c935783683b
xander wallace <xander wallace>
2017-02-07 19:38:46 -0800

    First commit

---
+++ src/NumberOne/WeAre.java
@@ -0,0 +1,10 @@
+package NumberOne;
+
+/**
+ * Created by Xander Wallace on 2/7/2017.
+ * Assignment for week 4 of CS133u
+ */
+public class WeAre {
+    System.out.println("Testing my first project.");
+    System.out.println("We are number one!");
+}
